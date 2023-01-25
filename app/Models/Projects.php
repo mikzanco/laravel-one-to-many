b53f148c7f27f0292bef77bc3c92454612326c85
mikzanco <mikyzancolo97@gmail.com>
2023-01-26 00:36:38 +0100

    first commit

---
+++ app/Models/Projects.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Projects extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'cover_image', 'summary', 'client_name', 'cover_image_original' ];
+
+    public static function generateSlug($string){
+
+        $slug = Str::slug($string, '-');
+
+        $original_slug = $slug;
+
+        $exists = Projects::where('slug', $slug)->first();
+
+        $c = 1;
+
+        while($exists){
+
+
+            $slug = $original_slug . '-' . $c;
+            $c++;
+
+            $exists = Projects::where('slug', $slug)->first();
+
+
+        }
+
+        return $slug;
+
+    }
+}
